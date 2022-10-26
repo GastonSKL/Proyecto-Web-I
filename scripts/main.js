7f6f96623143aff78086e0a05d407260fc411b31
GastonSKL <gastonlskamlec@gmail.com>
2022-10-26 01:26:13 -0300

    Se añade API
    Se añada intersection observer a div de skills

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,23 @@ botonCierreModal.addEventListener('click',()=>{
     modalNavBarBackground.style.display = 'none';
     modalNavbar.style.display = 'none';
 })
+
+const skills = document.querySelector('.skills__container');
+
+const cargarImagen = (entradas, observer) =>{
+    entradas.forEach(entrada => {
+        if(entrada.isIntersecting){
+            skills.style.animation = 'aparecer 1000ms';
+            skills.style.visibility = 'visible';
+            
+        }
+    });
+};
+
+const observer = new IntersectionObserver(cargarImagen,{
+    root: null,
+    rootMargin: '0px 0px 0px 0px',
+    threshold: .7
+});
+
+observer.observe(skills);
